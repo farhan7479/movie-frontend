0154592b35591413eb97e431972e3082a6b6c571
Farhan Shahid <123633657+farhan7479@users.noreply.github.com>
2024-10-27 22:22:00 +0530

    correct api url

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:8090',  // Added the missing colon
+    baseURL: 'https://movie-backend-java.onrender.com',  // Added the missing colon
     headers: { "ngrok-skip-browser-warning": "true" }
 });
